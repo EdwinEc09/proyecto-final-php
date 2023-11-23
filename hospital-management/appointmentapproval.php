9c13656971b27d353584766ffbf1194ce1e388ac
edwin <escorciacaballeroe@gmail.com>
2023-11-23 18:39:17 -0500

    se sube las alertas hasta departamento(departament)

--- hospital-management/appointmentapproval.php
+++ hospital-management/appointmentapproval.php
@@ -1,220 +1,250 @@
 <?php
 include("adheader.php");
 include("dbconnection.php");
-if(isset($_POST['submit']))
-{
-		if(isset($_GET['editid']))
-		{
-				$sql ="UPDATE patient SET status='Activo' WHERE patientid='$_GET[patientid]'";
-				$qsql=mysqli_query($con,$sql);
-			$roomid=0;
-			$sql ="UPDATE appointment SET appointmenttype='$_POST[apptype]',departmentid='$_POST[select5]',doctorid='$_POST[select6]',status='Aprobado',appointmentdate='$_POST[appointmentdate]',appointmenttime='$_POST[time]' WHERE appointmentid='$_GET[editid]'";
-			if($qsql = mysqli_query($con,$sql))
-			{
-				$roomid= $_POST['select3'];
-				$billtype = "Room Rent";
-				include("insertbillingrecord.php");				
-				echo "<script>alert('Registro de cita insertado exitosamente...');</script>";				
-				echo "<script>window.location='patientreport.php?patientid=$_GET[patientid]&appointmentid=$_GET[editid]';</script>";
-			}
-			else
-			{
-				echo mysqli_error($con);
-			}	
+if (isset($_POST['submit'])) {
+	if (isset($_GET['editid'])) {
+		$sql = "UPDATE patient SET status='Activo' WHERE patientid='$_GET[patientid]'";
+		$qsql = mysqli_query($con, $sql);
+		$roomid = 0;
+		$sql = "UPDATE appointment SET appointmenttype='$_POST[apptype]',departmentid='$_POST[select5]',doctorid='$_POST[select6]',status='Aprobado',appointmentdate='$_POST[appointmentdate]',appointmenttime='$_POST[time]' WHERE appointmentid='$_GET[editid]'";
+		if ($qsql = mysqli_query($con, $sql)) {
+			$roomid = $_POST['select3'];
+			$billtype = "Room Rent";
+			include("insertbillingrecord.php");
+			echo "<script>
+				Swal.fire({
+					title: '¡Exito!',
+					text: '¡Cita registrada exitosamente!',
+					icon: 'success'
+				  });
+				</script>";
+			echo "<script>window.location='patientreport.php?patientid=$_GET[patientid]&appointmentid=$_GET[editid]';</script>";
+		} else {
+			echo mysqli_error($con);
 		}
-		else
-		{
-			$sql ="UPDATE patient SET status='Activo' WHERE patientid='$_POST[select4]'";
-			$qsql=mysqli_query($con,$sql);
-				
-			$sql ="INSERT INTO appointment(appointmenttype,patientid,roomid,departmentid,appointmentdate,appointmenttime,doctorid,status) values('$_POST[select2]','$_POST[select4]','$_POST[select3]','$_POST[select5]','$_POST[appointmentdate]','$_POST[time]','$_POST[select6]','$_POST[select]')";
-			if($qsql = mysqli_query($con,$sql))
-			{
-				echo "<script>alert('Registro de cita insertado exitosamente...');</script>";
-			}
-			else
-			{
-				echo mysqli_error($con);
-			}
+	} else {
+		$sql = "UPDATE patient SET status='Activo' WHERE patientid='$_POST[select4]'";
+		$qsql = mysqli_query($con, $sql);
+
+		$sql = "INSERT INTO appointment(appointmenttype,patientid,roomid,departmentid,appointmentdate,appointmenttime,doctorid,status) values('$_POST[select2]','$_POST[select4]','$_POST[select3]','$_POST[select5]','$_POST[appointmentdate]','$_POST[time]','$_POST[select6]','$_POST[select]')";
+		if ($qsql = mysqli_query($con, $sql)) {
+			echo "<script>
+				Swal.fire({
+					title: '¡Exito!',
+					text: '¡Cita registrada exitosamente!',
+					icon: 'success'
+				  });
+				</script>";
+		} else {
+			echo mysqli_error($con);
 		}
+	}
 }
-if(isset($_GET['editid']))
-{
-	$sql="SELECT * FROM appointment WHERE appointmentid='$_GET[editid]' ";
-	$qsql = mysqli_query($con,$sql);
+if (isset($_GET['editid'])) {
+	$sql = "SELECT * FROM appointment WHERE appointmentid='$_GET[editid]' ";
+	$qsql = mysqli_query($con, $sql);
 	$rsedit = mysqli_fetch_array($qsql);
-	
+
 }
 ?>
 
 
 <div class="card ">
- 
-    <div class="block-header">
-	<h2 class="text-center">Proceso de aprobación de citas</h2>
-	</div>
-   <form method="post" action="" name="frmappnt" onSubmit="return validateform()">
-  
-    <table class="table table-striped">                
-        <tr>
-          <td >Paciente</td>
-          <td >
-            <?php
-			if(isset($_GET['patientid']))
-			{
-				$sqlpatient= "SELECT * FROM patient WHERE patientid='$_GET[patientid]'";
-				$qsqlpatient = mysqli_query($con,$sqlpatient);
-				$rspatient=mysqli_fetch_array($qsqlpatient);
-				echo $rspatient['patientname'] . " (Patient ID - $rspatient[patientid])";
-			}
-			else
-			{
-				$sqlpatient= "SELECT * FROM patient WHERE status='Activo'";
-				$qsqlpatient = mysqli_query($con,$sqlpatient);
-				while($rspatient=mysqli_fetch_array($qsqlpatient))
-				{
-					if($rspatient['patientid'] == $rsedit['patientid'])
-					{
-					echo "<option value='$rspatient[patientid]' selected> $rspatient[patientname](Patient ID - $rspatient[patientid])</option>";
-					}
-				}
-			}
-		  ?>
-      </td>
-        </tr>
 
-        <tr>
-          <td>Departamento</td>
-          <td><select name="select5" id="select5" class="form-control show-tick">
-           <option value="">Seleccionar</option>
-            <?php
-		  	$sqldepartment= "SELECT * FROM department WHERE status='Activo'";
-			$qsqldepartment = mysqli_query($con,$sqldepartment);
-			while($rsdepartment=mysqli_fetch_array($qsqldepartment))
-			{
-				if($rsdepartment['departmentid'] == $rsedit['departmentid'])
-				{
-	echo "<option value='$rsdepartment[departmentid]' selected>$rsdepartment[departmentname]</option>";
-				}
-				else
-				{
-  echo "<option value='$rsdepartment[departmentid]'>$rsdepartment[departmentname]</option>";
-				}
-				
-			}
-		  ?>
-          </select></td>
-        </tr>
-		
-        <tr>
-          <td>Doctor</td>
-          <td><select name="select6" id="select6" class="form-control show-tick">
-            <option value="">Seleccionar</option>
-            <?php
-          	$sqldoctor= "SELECT * FROM doctor INNER JOIN department ON department.departmentid=doctor.departmentid WHERE doctor.status='Activo'";
-			$qsqldoctor = mysqli_query($con,$sqldoctor);
-			while($rsdoctor = mysqli_fetch_array($qsqldoctor))
-			{
-				if($rsdoctor['doctorid'] == $rsedit['doctorid'])
-				{
-					echo "<option value='$rsdoctor[doctorid]' selected>$rsdoctor[doctorname] ( $rsdoctor[departmentname] ) </option>";
-				}
-				else
-				{
-					echo "<option value='$rsdoctor[doctorid]'>$rsdoctor[doctorname] ( $rsdoctor[departmentname] )</option>";				
-				}
-			}
-		  ?>
-          </select></td>
-        </tr>
-		
-        <tr>
-          <td>Día de la cita</td>
-          <td><input class="form-control" type="date" name="appointmentdate" id="appointmentdate" value="<?php echo $rsedit['appointmentdate']; ?>" /></td>
-        </tr>
-        <tr>
-          <td>Hora de la cita</td>
-          <td><input class="form-control" type="time" name="time" id="time" value="<?php echo $rsedit['appointmenttime']; ?>" /></td>
-        </tr>
-        <tr>
-          <td>Motivo de la cita</td>
-          <td><input class="form-control" name="appreason" id="appreason" value="<?php echo $rsedit['app_reason']; ?>"/></td>         
-        </tr>
-        <tr>
-          <td colspan="2" align="center"><input class="btn btn-default" type="submit" name="submit" id="submit" value="Entregar" /></td>
-        </tr>
-      </tbody>
-    </table>
-    </form>
-    <p>&nbsp;</p>
-  </div>
+	<div class="block-header">
+		<h2 class="text-center">Proceso de aprobación de citas</h2>
+	</div>
+	<form method="post" action="" name="frmappnt" onSubmit="return validateform()">
+
+		<table class="table table-striped">
+			<tr>
+				<td>Paciente</td>
+				<td>
+					<?php
+					if (isset($_GET['patientid'])) {
+						$sqlpatient = "SELECT * FROM patient WHERE patientid='$_GET[patientid]'";
+						$qsqlpatient = mysqli_query($con, $sqlpatient);
+						$rspatient = mysqli_fetch_array($qsqlpatient);
+						echo $rspatient['patientname'] . " (Patient ID - $rspatient[patientid])";
+					} else {
+						$sqlpatient = "SELECT * FROM patient WHERE status='Activo'";
+						$qsqlpatient = mysqli_query($con, $sqlpatient);
+						while ($rspatient = mysqli_fetch_array($qsqlpatient)) {
+							if ($rspatient['patientid'] == $rsedit['patientid']) {
+								echo "<option value='$rspatient[patientid]' selected> $rspatient[patientname](Patient ID - $rspatient[patientid])</option>";
+							}
+						}
+					}
+					?>
+				</td>
+			</tr>
+
+			<tr>
+				<td>Departamento</td>
+				<td><select name="select5" id="select5" class="form-control show-tick">
+						<option value="">Seleccionar</option>
+						<?php
+						$sqldepartment = "SELECT * FROM department WHERE status='Activo'";
+						$qsqldepartment = mysqli_query($con, $sqldepartment);
+						while ($rsdepartment = mysqli_fetch_array($qsqldepartment)) {
+							if ($rsdepartment['departmentid'] == $rsedit['departmentid']) {
+								echo "<option value='$rsdepartment[departmentid]' selected>$rsdepartment[departmentname]</option>";
+							} else {
+								echo "<option value='$rsdepartment[departmentid]'>$rsdepartment[departmentname]</option>";
+							}
+
+						}
+						?>
+					</select></td>
+			</tr>
+
+			<tr>
+				<td>Doctor</td>
+				<td><select name="select6" id="select6" class="form-control show-tick">
+						<option value="">Seleccionar</option>
+						<?php
+						$sqldoctor = "SELECT * FROM doctor INNER JOIN department ON department.departmentid=doctor.departmentid WHERE doctor.status='Activo'";
+						$qsqldoctor = mysqli_query($con, $sqldoctor);
+						while ($rsdoctor = mysqli_fetch_array($qsqldoctor)) {
+							if ($rsdoctor['doctorid'] == $rsedit['doctorid']) {
+								echo "<option value='$rsdoctor[doctorid]' selected>$rsdoctor[doctorname] ( $rsdoctor[departmentname] ) </option>";
+							} else {
+								echo "<option value='$rsdoctor[doctorid]'>$rsdoctor[doctorname] ( $rsdoctor[departmentname] )</option>";
+							}
+						}
+						?>
+					</select></td>
+			</tr>
+
+			<tr>
+				<td>Día de la cita</td>
+				<td><input class="form-control" type="date" name="appointmentdate" id="appointmentdate"
+						value="<?php echo $rsedit['appointmentdate']; ?>" /></td>
+			</tr>
+			<tr>
+				<td>Hora de la cita</td>
+				<td><input class="form-control" type="time" name="time" id="time"
+						value="<?php echo $rsedit['appointmenttime']; ?>" /></td>
+			</tr>
+			<tr>
+				<td>Motivo de la cita</td>
+				<td><input class="form-control" name="appreason" id="appreason"
+						value="<?php echo $rsedit['app_reason']; ?>" /></td>
+			</tr>
+			<tr>
+				<td colspan="2" align="center"><input class="btn btn-default" type="submit" name="submit" id="submit"
+						value="Entregar" /></td>
+			</tr>
+			</tbody>
+		</table>
+	</form>
+	<p>&nbsp;</p>
+</div>
 
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <?php
 include("adfooter.php");
 ?>
 <script type="application/javascript">
-function validateform()
-{
-	if(document.frmappnt.select4.value == "")
-	{
-		alert("Patient name should not be empty..");
-		document.frmappnt.select4.focus();
-		return false;
+	function validateform() {
+		if (document.frmappnt.select4.value == "") {
+			// alert("Patient name should not be empty..");
+			Swal.fire({
+                position: 'top-center',
+                icon: 'error',
+                title: 'Nombre de Paciente no debe ir vacio.',
+                showConfirmButton: false,
+                timer: 2000,
+            });
+			document.frmappnt.select4.focus();
+			return false;
+		}
+		else if (document.frmappnt.select3.value == "") {
+			// alert("Room type should not be empty..");
+			Swal.fire({
+                position: 'top-center',
+                icon: 'error',
+                title: 'El tipo de habitación no debe estar vacía..',
+                showConfirmButton: false,
+                timer: 2000,
+            });
+			document.frmappnt.select3.focus();
+			return false;
+		}
+		else if (document.frmappnt.select5.value == "") {
+			// alert("Department name should not be empty..");
+			Swal.fire({
+                position: 'top-center',
+                icon: 'error',
+                title: 'Departamento no debe ir vacio.',
+                showConfirmButton: false,
+                timer: 2000,
+            });
+			document.frmappnt.select5.focus();
+			return false;
+		}
+		else if (document.frmappnt.appointmentdate.value == "") {
+			// alert("Appointment date should not be empty..");
+			Swal.fire({
+                position: 'top-center',
+                icon: 'error',
+                title: 'fecha no debe ir vacio',
+                showConfirmButton: false,
+                timer: 2000,
+            });
+			document.frmappnt.appointmentdate.focus();
+			return false;
+		}
+		else if (document.frmappnt.time.value == "") {
+			// alert("Appointment time should not be empty..");
+			Swal.fire({
+                position: 'top-center',
+                icon: 'error',
+                title: 'hora no debe ir vacio.',
+                showConfirmButton: false,
+                timer: 2000,
+            });
+			document.frmappnt.time.focus();
+			return false;
+		}
+		else if (document.frmappnt.select6.value == "") {
+			// alert("Doctor name should not be empty..");
+			Swal.fire({
+                position: 'top-center',
+                icon: 'error',
+                title: 'nombre de doctor no debe ir vacio',
+                showConfirmButton: false,
+                timer: 2000,
+            });
+			document.frmappnt.select6.focus();
+			return false;
+		}
+		else if (document.frmappnt.select.value == "") {
+			// alert("Kindly select the status..");
+			Swal.fire({
+                position: 'top-center',
+                icon: 'error',
+                title: 'estado no debe ir vacio.',
+                showConfirmButton: false,
+                timer: 2000,
+            });
+			document.frmappnt.select.focus();
+			return false;
+		}
+		else {
+			return true;
+		}
 	}
-	else if(document.frmappnt.select3.value == "")
-	{
-		alert("Room type should not be empty..");
-		document.frmappnt.select3.focus();
-		return false;
-	}
-	else if(document.frmappnt.select5.value == "")
-	{
-		alert("Department name should not be empty..");
-		document.frmappnt.select5.focus();
-		return false;
-	}
-	else if(document.frmappnt.appointmentdate.value == "")
-	{
-		alert("Appointment date should not be empty..");
-		document.frmappnt.appointmentdate.focus();
-		return false;
-	}
-	else if(document.frmappnt.time.value == "")
-	{
-		alert("Appointment time should not be empty..");
-		document.frmappnt.time.focus();
-		return false;
-	}
-	else if(document.frmappnt.select6.value == "")
-	{
-		alert("Doctor name should not be empty..");
-		document.frmappnt.select6.focus();
-		return false;
-	}
-	else if(document.frmappnt.select.value == "" )
-	{
-		alert("Kindly select the status..");
-		document.frmappnt.select.focus();
-		return false;
-	}
-	else
-	{
-		return true;
-	}
-}
-$('.out_patient').hide();
-$('#apptype').change(function()
-{
-	apptype=$('#apptype').val();
-	if(apptype=='InPatient')
-	{
-		$('.out_patient').show();
-	}
-	else
-	{
-		$('.out_patient').hide();
-	}
-});
+	$('.out_patient').hide();
+	$('#apptype').change(function () {
+		apptype = $('#apptype').val();
+		if (apptype == 'InPatient') {
+			$('.out_patient').show();
+		}
+		else {
+			$('.out_patient').hide();
+		}
+	});
 </script>
--- hospital-management/department.php
+++ hospital-management/department.php
@@ -1,128 +1,147 @@
 <?php
 include("adheader.php");
 include("dbconnection.php");
-if(isset($_POST['submit']))
-{
-	if(isset($_GET['editid']))
-	{
-			$sql ="UPDATE department SET departmentname='$_POST[departmentname]',description='$_POST[textarea]',status='$_POST[select]' WHERE departmentid='$_GET[editid]'";
-		if($qsql = mysqli_query($con,$sql))
-		{
-			echo "<script>alert('Registro del departamento actualizado exitosamente.');</script>";
-		}
-		else
-		{
+if (isset($_POST['submit'])) {
+	if (isset($_GET['editid'])) {
+		$sql = "UPDATE department SET departmentname='$_POST[departmentname]',description='$_POST[textarea]',status='$_POST[select]' WHERE departmentid='$_GET[editid]'";
+		if ($qsql = mysqli_query($con, $sql)) {
+			echo "<script>
+			  
+            Swal.fire({
+                title: '¡Exito!',
+                text: '¡departamento actualizado exitosamente!',
+                icon: 'success'
+              });
+			</script>";
+		} else {
 			echo mysqli_error($con);
-		}	
-	}
-	else
-	{
-	$sql ="INSERT INTO department(departmentname,description,status) values('$_POST[departmentname]','$_POST[textarea]','$_POST[select]')";
-	if($qsql = mysqli_query($con,$sql))
-	{
-		echo "<script>alert('Registro de departamento insertado exitosamente.');</script>";
-	}
-	else
-	{
-		echo mysqli_error($con);
+		}
+	} else {
+		$sql = "INSERT INTO department(departmentname,description,status) values('$_POST[departmentname]','$_POST[textarea]','$_POST[select]')";
+		if ($qsql = mysqli_query($con, $sql)) {
+			echo "<script>
+		Swal.fire({
+			title: '¡Exito!',
+			text: '¡departamento registrado exitosamente!',
+			icon: 'success'
+		  });
+		</script>";
+		} else {
+			echo mysqli_error($con);
+		}
 	}
 }
-}
-if(isset($_GET['editid']))
-{
-	$sql="SELECT * FROM department WHERE departmentid='$_GET[editid]' ";
-	$qsql = mysqli_query($con,$sql);
+if (isset($_GET['editid'])) {
+	$sql = "SELECT * FROM department WHERE departmentid='$_GET[editid]' ";
+	$qsql = mysqli_query($con, $sql);
 	$rsedit = mysqli_fetch_array($qsql);
-	
+
 }
 ?>
 
 
 <div class="container-fluid">
 	<div class="block-header">
-            <h2 class="text-center">Agregar nuevo departamento</h2>
-            
-        </div>
-  <div class="card">
+		<h2 class="text-center">Agregar nuevo departamento</h2>
 
-    <form method="post" action="" name="frmdept" onSubmit="return validateform()">
-    <table class="table table-hover">
-      <tbody>
-        <tr>
-          <td width="34%">Nombre de Departamento</td>
-          <td width="66%"><input placeholder=" Entre aquí " class="form-control" type="text" name="departmentname" id="departmentname" value="<?php echo $rsedit['departmentname']; ?>" /></td>
-        </tr>
-        <tr>
-          <td>Descripción</td>
-          <td><textarea placeholder=" Entre aquí " class="form-control no-resize" name="textarea" id="textarea" cols="45" rows="5"><?php echo $rsedit['description'] ; ?></textarea></td>
-        </tr>
-        <tr>
-          <td>Estado</td>
-          <td> <select name="select" id="select" class="form-control show-tick">
-            <option value="">Seleccionar</option>
-            <?php
-		  $arr = array("Activo","Inactivo");
-		  foreach($arr as $val)
-		  {
-			  if($val == $rsedit['status'])
-			  {
-			  echo "<option value='$val' selected>$val</option>";
-			  }
-			  else
-			  {
-				  echo "<option value='$val'>$val</option>";			  
-			  }
-		  }
-		  ?>
-            </select></td>
-        </tr>
-        <tr>
-          <td colspan="2" align="center"><input class="btn btn-default" type="submit" name="submit" id="submit" value="Entregar" /></td>
-        </tr>
-      </tbody>
-    </table>
-    </form>
-    <p>&nbsp;</p>
-  </div>
+	</div>
+	<div class="card">
+
+		<form method="post" action="" name="frmdept" onSubmit="return validateform()">
+			<table class="table table-hover">
+				<tbody>
+					<tr>
+						<td width="34%">Nombre de Departamento</td>
+						<td width="66%"><input placeholder=" Entre aquí " class="form-control" type="text"
+								name="departmentname" id="departmentname"
+								value="<?php echo $rsedit['departmentname']; ?>" /></td>
+					</tr>
+					<tr>
+						<td>Descripción</td>
+						<td><textarea placeholder=" Entre aquí " class="form-control no-resize" name="textarea"
+								id="textarea" cols="45" rows="5"><?php echo $rsedit['description']; ?></textarea></td>
+					</tr>
+					<tr>
+						<td>Estado</td>
+						<td> <select name="select" id="select" class="form-control show-tick">
+								<option value="">Seleccionar</option>
+								<?php
+								$arr = array("Activo", "Inactivo");
+								foreach ($arr as $val) {
+									if ($val == $rsedit['status']) {
+										echo "<option value='$val' selected>$val</option>";
+									} else {
+										echo "<option value='$val'>$val</option>";
+									}
+								}
+								?>
+							</select></td>
+					</tr>
+					<tr>
+						<td colspan="2" align="center"><input class="btn btn-default" type="submit" name="submit"
+								id="submit" value="Entregar" /></td>
+					</tr>
+				</tbody>
+			</table>
+		</form>
+		<p>&nbsp;</p>
+	</div>
 </div>
 </div>
- <div class="clear"></div>
-  </div>
+<div class="clear"></div>
+</div>
 </div>
 <?php
 include("adfooter.php");
 ?>
 <script type="application/javascript">
-var alphaExp = /^[a-zA-Z]+$/; //Variable to validate only alphabets
-var alphaspaceExp = /^[a-zA-Z\s]+$/; //Variable to validate only alphabets and space
-var numericExpression = /^[0-9]+$/; //Variable to validate only numbers
-var alphanumericExp = /^[0-9a-zA-Z]+$/; //Variable to validate numbers and alphabets
-var emailExp = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/; //Variable to validate Email ID 
+	var alphaExp = /^[a-zA-Z]+$/; //Variable to validate only alphabets
+	var alphaspaceExp = /^[a-zA-Z\s]+$/; //Variable to validate only alphabets and space
+	var numericExpression = /^[0-9]+$/; //Variable to validate only numbers
+	var alphanumericExp = /^[0-9a-zA-Z]+$/; //Variable to validate numbers and alphabets
+	var emailExp = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/; //Variable to validate Email ID 
 
-function validateform()
-{
-	if(document.frmdept.departmentname.value == "")
-	{
-		alert("El nombre del departamento no debe estar vacío.");
-		document.frmdept.departmentname.focus();
-		return false;
+	function validateform() {
+		if (document.frmdept.departmentname.value == "") {
+			// alert("El nombre del departamento no debe estar vacío.");
+			Swal.fire({
+                position: 'top-center',
+                icon: 'error',
+                title: 'Nombre de departamento no debe ir vacio.',
+                showConfirmButton: false,
+                timer: 2000,
+            });
+
+			document.frmdept.departmentname.focus();
+			return false;
+		}
+		else if (!document.frmdept.departmentname.value.match(alphaspaceExp)) {
+			// alert("Nombre del departamento no válido.");
+			Swal.fire({
+                position: 'top-center',
+                icon: 'error',
+                title: 'Nombre de departamento no valido.',
+                showConfirmButton: false,
+                timer: 2000,
+            });
+			document.frmdept.departmentname.focus();
+			return false;
+		}
+		else if (document.frmdept.select.value == "") {
+			// alert("Por favor seleccione el estado.");
+			Swal.fire({
+                position: 'top-center',
+                icon: 'error',
+                title: 'estado no debe ir vacio.',
+                showConfirmButton: false,
+                timer: 2000,
+            });
+			document.frmdept.select.focus();
+			return false;
+		}
+
+		else {
+			return true;
+		}
 	}
-	else if(!document.frmdept.departmentname.value.match(alphaspaceExp))
-	{
-		alert("Nombre del departamento no válido.");
-		document.frmdept.departmentname.focus();
-		return false;
-	}
-	else if(document.frmdept.select.value == "" )
-	{
-		alert("Por favor seleccione el estado.");
-		document.frmdept.select.focus();
-		return false;
-	}
-	
-	else
-	{
-		return true;
-	}
-}
 </script>
